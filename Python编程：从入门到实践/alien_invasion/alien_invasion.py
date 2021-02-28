e8084d6c9f9578f5e61097316252cb35f885cce7
graigcxz <graigcxz@gmail.com>
2021-02-28 23:36:37 +0800

    28日23:35，目前完成了13.4，但是有个问题就是机器人跑出右边界了，明天中午重新看一下哪里出问题了

--- Python编程：从入门到实践/alien_invasion/alien_invasion.py
+++ Python编程：从入门到实践/alien_invasion/alien_invasion.py
@@ -1,6 +1,8 @@
 import pygame
+from pygame.sprite import Group
 
 import game_functions as gf
+from alien import Alien
 from settings import Settings
 from ship import Ship
 
@@ -13,15 +15,22 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 创建一艘飞船
+    # 创建一艘飞船、一个用于存储子弹的编组和一个外星人编组
     ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- Python编程：从入门到实践/alien_invasion/settings.py
+++ Python编程：从入门到实践/alien_invasion/settings.py
@@ -11,3 +11,15 @@ class Settings:
 
         # 飞船的设置
         self.ship_speed_factor = 0.5
+
+        # 子弹的设置
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (0, 0, 0)
+        self.bullet_speed_factor = 0.3
+        self.bullets_allowed = 3
+
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 0.2
+        self.fleet_direction = 1

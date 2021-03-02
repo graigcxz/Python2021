9c1dd8c1658ed155bc34e023c0dfaa13932f1d0a
graigcxz <graigcxz@gmail.com>
2021-03-02 14:36:30 +0800

    March 2, 14:36，完成15.3，差练习题

---
+++ Python编程：从入门到实践/Chapter15/test15-2.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+# 15-2 彩色立方
+
+import matplotlib.pyplot as plt
+
+x_values = list(range(1, 5000))
+y_values = [x ** 3 for x in x_values]
+
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds,
+            edgecolors='none', s=40)
+
+# 设置图表标题，并给坐标轴加上标签
+plt.title("Cubic Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=24)
+plt.ylabel("Cubic of Value", fontsize=24)
+
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+# 设置每个坐标轴的取值范围
+# plt.axis([0, 6, 0, 150])
+
+plt.show()
+# plt.savefig('squares_plt.png', bbox_inches='tight')
